ec8405b2a2c784a029cba5bbfd5b03242432f967
aksoftware98 <aksoftware98@hotmail.com>
2020-04-16 21:20:06 +0300

    Init the project

---
+++ src/BlazorCashier/BlazorCashier.Client.Blazor/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorCashier.Client.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                // Configure your authentication provider options here.
+                // For more information, see https://aka.ms/blazor-standalone-auth
+                options.ProviderOptions.Authority = "https://login.microsoftonline.com/";
+                options.ProviderOptions.ClientId = "33333333-3333-3333-33333333333333333";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/BlazorCashier/BlazorCashier.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlazorCashier.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
